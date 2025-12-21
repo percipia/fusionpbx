d448c55fb869996eb280ab1b500413eb53b23b35
markjcrane <markjcrane@gmail.com>
2025-12-21 00:21:01 -0700

    Simplify the update_indexes

--- core/upgrade/upgrade.php
+++ core/upgrade/upgrade.php
@@ -241,12 +241,7 @@
 		}
 
 		//update database foreign key indexes
-		$response = $database->update_indexes();
-		if ($display_type === 'text') {
-			foreach($response as $row) {
-				echo "        ".trim($row['sql'])."\n";
-			}
-		}
+		$database->update_indexes();
 	}
 
 //upgrade schema and/or data_types
